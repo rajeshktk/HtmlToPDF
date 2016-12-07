58825d42cccc456d7243d92c64941c24634f2453
is36 <rajesh.thondamkannath@in.tesco.com>
2016-12-07 09:31:49 +0000

    first commit

---
+++ HtmlToPDF/Controllers/ConvertController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+using System.Drawing;
+
+namespace HtmlToPDF.Controllers
+{
+    public class ConvertController : Controller
+    {
+        // GET: Convert
+        public ActionResult Index()
+        {
+            return View();
+        }
+     
+    }
+}
---
+++ HtmlToPDF/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿
+using SelectPdf;
+using System.Web.Mvc;
+
+namespace HtmlToPDF.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        public ActionResult RajeshCV()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public void ToPDF()
+        {
+            string url = "http://localhost:64501/Home/RajeshCV";
+            string file = @"\\global.tesco.org\dfsroot\IN\HeadOffice\Home\BLR01\IS36\Desktop\Rajesh.pdf";
+
+            HtmlToPdf converter = new HtmlToPdf();
+            // convert the url to pdf
+            PdfDocument doc = converter.ConvertUrl(url);
+
+            // save pdf document
+            doc.Save(file);
+
+            // close pdf document
+            doc.Close();
+          
+        }
+    }
+}
